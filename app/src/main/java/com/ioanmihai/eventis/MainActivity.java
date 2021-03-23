46c082e9f27cba71bdfd509ce6e0b0ab8da602fb
IoanMihai <mihaioan2005@gmail.com>
2021-03-23 22:44:35 +0200

    Ceva micut

--- app/src/main/java/com/ioanmihai/eventis/MainActivity.java
+++ app/src/main/java/com/ioanmihai/eventis/MainActivity.java
@@ -1,14 +1,25 @@
 package com.ioanmihai.eventis;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
-    public TextView txtName;
-    public TextView pbn;
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private RecyclerView eventList;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private FloatingActionButton floatingActionButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
