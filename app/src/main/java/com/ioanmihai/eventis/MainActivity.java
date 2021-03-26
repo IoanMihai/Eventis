0dac374aeff8535a3656d8da900baf7fa9b2940f
AndreiBZV <andreibazavan25@gmail.com>
2021-03-26 19:55:00 +0200

    test

--- app/src/main/java/com/ioanmihai/eventis/MainActivity.java
+++ app/src/main/java/com/ioanmihai/eventis/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView eventList;
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private FloatingActionButton floatingActionButton;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
