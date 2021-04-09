db7389e2ea7e12baab1bfb03409c1312c50917bc
IoanMihai <mihaioan2005@gmail.com>
2021-04-09 21:45:26 +0300

    Nimic special

--- app/src/main/java/com/ioanmihai/eventis/MainActivity.java
+++ app/src/main/java/com/ioanmihai/eventis/MainActivity.java
@@ -2,19 +2,29 @@ package com.ioanmihai.eventis;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private FloatingActionButton floatingActionButton;
     private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef;
+
+    private String currentUserID;
+    private TextView NavProfileUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +54,29 @@ public class MainActivity extends AppCompatActivity {
         actionBarDrawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        currentUserID = mAuth.getCurrentUser().getUid();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        NavProfileUserName = (TextView) navView.findViewById(R.id.nav_fullname);
+
+        UsersRef.child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    if (snapshot.hasChild("fullname")) {
+                        String FullName = snapshot.child("fullname").getValue().toString();
+                        NavProfileUserName.setText(FullName);
+                    }else{
+                        Toast.makeText(MainActivity.this, "Fara nume, fara CNP", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -47,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
     }
 
     @Override
@@ -65,12 +103,30 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.nav_past_events:
                 break;
+            case R.id.nav_about:
+                showAlert();
+                break;
             default:
                 mAuth.signOut();
                 SendUserToRegisterActivity();
                 break;
         }
     }
+
+    private void showAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this)
+                .setTitle("About")
+                .setMessage("Develpoed and created by Ioan Mihai and Bazavan Andrei")
+                .setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        builder.create().show();
+    }
+
 
     private void SendUserToRegisterActivity() {
         Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
